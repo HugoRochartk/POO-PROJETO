be6ce2cf51ccb1218da48e2528a7614e4c584251
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2022-04-07 15:59:36 +0100

    Create SmartSpeaker.java

---
+++ Projeto/SmartSpeaker.java
@@ -0,0 +1,61 @@
+/**
+ * Um SmartSpeaker é um SmartDevice que além de ligar e desligar permite também
+ * reproduzir som.
+ * Consegue ligar-se a um canal (por simplificação uma rádio online) e permite
+ * a regulação do seu nível de volume.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class SmartSpeaker extends SmartDevice {
+    public static final int MAX = 20; //volume máximo
+    
+    private int volume;
+    private String channel;
+
+
+    /**
+     * Constructor for objects of class SmartSpeaker
+     */
+    public SmartSpeaker() {
+        super();
+        this.channel = "";
+        this.volume = 10;
+    }
+
+    public SmartSpeaker(String s) {
+        super(s);
+        this.channel = "";
+        this.volume = 10;
+    }
+
+
+
+    public SmartSpeaker(String cod, String channel, int i) {
+        super(cod);
+        this.channel = channel;
+        this.volume = i;
+    }
+
+    public void volumeUp() {
+        if (this.volume<MAX) this.volume++;
+    }
+
+    public void volumeDown() {
+        if (this.volume>0) this.volume--;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+    
+    public String getChannel() {
+        return this.channel;
+    }
+    
+    public void setChannel(String c) {
+        this.channel = c;
+    }
+
+}
