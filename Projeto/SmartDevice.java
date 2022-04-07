bd65045660e95732321eef84434e6ef087c27ed3
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2022-04-07 15:59:01 +0100

    Create SmartDevice.java

---
+++ Projeto/SmartDevice.java
@@ -0,0 +1,59 @@
+/**
+ * A classe SmartDevice é um contactor simples.
+ * Permite ligar ou desligar circuitos. 
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class SmartDevice {
+
+    private String id;
+    private boolean on;
+
+    /**
+     * Constructor for objects of class SmartDevice
+     */
+    public SmartDevice() {
+        this.id = "";
+        this.on = false;
+    }
+
+
+    public SmartDevice(String s) {
+        this.id = s;
+        this.on = false;
+    }
+
+
+    public SmartDevice(String s, boolean b) {
+        this.id = s;
+        this.on = b;
+    }
+
+    public SmartDevice clone(){
+        return new SmartDevice(this.id, this.on);
+
+    }
+
+    public void turnOn() {
+        this.on = true;
+    }
+    
+    public void turnOff() {
+        this.on = false;
+    }
+    
+    public boolean getOn() {
+        return this.on;
+    }
+    
+    public void setOn(boolean b) {
+        this.on = b;
+    }
+    
+    public String getID() {
+        return this.id;
+    }
+
+}
