4950abe1441974af015b140a31cb32f41b2e3cb8
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2022-04-18 22:00:09 +0100

    Update SmartCamera.java

--- Projeto/SmartCamera.java
+++ Projeto/SmartCamera.java
@@ -1,6 +1,20 @@
 public class SmartCamera extends SmartDevice {
 
-    private String resolucao;  //exemplo: "1280:720" HD, "2048:1080" 2K
+    private String resolucao;  
+    /*- VGA: 640 x 480 pixels;
+      - QVGA: 320 x 240 pixels;
+      - WVGA: 800 x 480 pixels;
+      - SVGA / Super VGA: 800 x 600 pixels;
+      - XGA: 1024 x 768 pixels;
+      - WXGA: de 1152 x 768 pixels a 1366 x 768 pixels;
+      - HD / 720p : 1280 x 720 pixels;
+      - qHD: 960 x 540 pixels;
+      - full HD (FHD ou 1080p): 1920 x 1080 pixels;
+      - QHD (WQHD): 2560 x 1440 pixels.
+      - 2K: 2048 x 1080 pixels;
+      - 4K (UHDTV ou QFHD): 3840 x 2160 pixels;
+      - 8K: de 7680 x 4320 pixels a 10080 x 4320 pixels (depende da proporção do aparelho).
+    */
     private int tamanho;
 
 
