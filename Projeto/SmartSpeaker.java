e37bc8df08e1f73516891041641683d2bc6589de
xDwake <gabi.alex.sil2002@gmail.com>
2022-04-26 18:19:03 +0100

    update

--- Projeto/SmartSpeaker.java
+++ Projeto/SmartSpeaker.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -14,29 +15,33 @@ public class SmartSpeaker extends SmartDevice {
     public static final int MAX = 20; //volume máximo
     
     
-    private String marca;
+    private static final int JBL = 1; // 480 Watt dia
+    private static final int MARSHALL = 2; // 600 Watt dia
+    private static final int BOSE = 3; // 540 Watt dia
     private int volume;
     private String channel;
-
+    private int marca;
 
     /**
      * Constructor for objects of class SmartSpeaker
      */
     public SmartSpeaker() {
         super();
+        this.marca = 0;
         this.channel = "";
         this.volume = 10;
     }
 
     public SmartSpeaker(String s) {
         super(s);
+        this.marca = 0;
         this.channel = "";
         this.volume = 10;
     }
 
 
 
-    public SmartSpeaker(String cod, String marca, String channel, int i) {
+    public SmartSpeaker(String cod, int marca, String channel, int i) {
         super(cod);
         this.marca = marca;
         this.channel = channel;
@@ -51,14 +56,14 @@ public class SmartSpeaker extends SmartDevice {
         if (this.volume>0) this.volume--;
     }
     
-    public String getMarca(){
+    public int getMarca(){
         return this.marca;
     }
 
-    public void setMarca(String marca){
+    public void setMarca(int marca){
         this.marca = marca;
     }
-    
+
 
     public int getVolume() {
         return this.volume;
@@ -76,6 +81,28 @@ public class SmartSpeaker extends SmartDevice {
         if (this == o) return true;
         if (o == null || this.getClass() != o.getClass()) return false;
         SmartSpeaker that = (SmartSpeaker) o;
-        return (this.volume == that.volume && this.marca.equalsIgnoreCase(that.marca) && this.channel.equalsIgnoreCase(that.channel));
+        return (this.volume == that.volume && this.marca == that.marca && this.channel.equalsIgnoreCase(that.channel));
+    }
+
+    public String toString() {
+        return "SmartSpeaker{" +
+                "volume=" + volume +
+                ", channel='" + channel + '\'' +
+                ", marca=" + marca +
+                '}';
+    }
+
+    public void CalculaConsumoEnergetico(){ //TODO SO PARA CLARIFICAR !!! neste caso como esta classe nao recebe o valor do consumo da lampada como variavel de instancia esse passa a ser o valor arbitrario e os valores passados a MARSHALL, BOSE e JBL sao agora os fatores multiplicativos do volume.(neste caso nao faz tant sentido mas foi pra facilitar)
+        switch (this.marca){
+            case MARSHALL -> {
+                super.ConsumoDiarioEN = 600 + (MARSHALL * this.volume) ;
+            }
+            case BOSE -> {
+                super.ConsumoDiarioEN = 540 + (BOSE * this.volume);
+            }
+            default -> {
+                super.ConsumoDiarioEN = 480 + (JBL * this.volume);
+            }
+        }
     }
 }
