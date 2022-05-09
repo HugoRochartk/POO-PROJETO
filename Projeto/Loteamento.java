fbb6ab0760c4153b7e197f09d3567ed673fcd35e
Hugo Rocha <hugorochartk@gmail.com>
2022-05-10 00:52:30 +0100

    peqs alteraçoes, mais uns metodos

--- Projeto/Loteamento.java
+++ Projeto/Loteamento.java
@@ -1,25 +1,50 @@
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class Loteamento {
-    private Map<String, CasaInteligente> loteamento;
+    private Map<Integer, ArrayList<CasaInteligente>> loteamento;  //  (nif, listadascasasquepossui)
    
     
     public Loteamento(){
-        this.loteamento = new HashMap();  //ta que te pariu oh ze diogo, e o construtor vazio????
+        this.loteamento = new HashMap<>();
     }
     
-    public Loteamento(Map<String, CasaInteligente> loteamento) {
+    public Loteamento(Map<Integer, ArrayList<CasaInteligente>> loteamento) {
         this.loteamento = loteamento;
     }
 
-    public Map<String, CasaInteligente> getLoteamento() {
-        return loteamento;
+    public Map<Integer, ArrayList<CasaInteligente>> getLoteamento() {
+        return this.loteamento;
     }
 
-    public void setLoteamento(Map<String, CasaInteligente> loteamento) {
+    public void setLoteamento(Map<Integer, ArrayList<CasaInteligente>> loteamento) {
         this.loteamento = loteamento;
     }
+
+    public void addCasaInteligente(CasaInteligente c){
+         if(this.loteamento.containsKey(c.getNif())){
+            this.loteamento.get(c.getNif()).add(c.clone());
+         }
+         else{
+             ArrayList<CasaInteligente> lista = new ArrayList<>();
+             this.loteamento.put(c.getNif(), lista.add(c.clone());
+         }
+
+    }
+
+
+    public ArrayList<CasaInteligente> getCasasInteligentes(int nif){  //dada uma pessoa retorna as casas que lhe pertencem
+        return this.loteamento.get(nif);
+    }
+
+
+    public int getNif(CasaInteligente c){
+
+        for(Map.Entry<Integer, ArrayList<CasaInteligente>> pair: this.loteamento.entrySet()){
+            if(pair.getValue().contains(c)) return pair.getKey();
+        }
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
