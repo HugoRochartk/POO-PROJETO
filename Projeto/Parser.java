2dc8c906fc0252f31df9509c2292d22ff9cc8c4e
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2022-05-17 14:09:50 +0100

    Update Parser.java

--- Projeto/Parser.java
+++ Projeto/Parser.java
@@ -24,17 +24,30 @@ public class Parser {
                 case "Divisao":
                     if (casaMaisRecente == null) System.out.println("Linha inválida.");
                     divisao = linhaPartida[1];
+                    assert casaMaisRecente != null;
                     casaMaisRecente.addRoom(divisao);
-                    (...)
                     break;
                 case "SmartBulb":
                     if (divisao == null) System.out.println("Linha inválida.");
                     SmartBulb sd = parseSmartBulb(linhaPartida[1]);
+                    assert casaMaisRecente != null;
                     casaMaisRecente.addDevice(sd);
                     casaMaisRecente.addToRoom(divisao, sd.getID());
-                    (...)
                     break;
-                (...)
+                case "SmartSpeaker":
+                    if (divisao == null) System.out.println("Linha inválida.");
+                    SmartSpeaker ss = parseSmartSpeaker(linhaPartida[1]);
+                    assert casaMaisRecente != null;
+                    casaMaisRecente.addDevice(ss);
+                    casaMaisRecente.addToRoom(divisao, ss.getID());
+                    break;
+                case "SmartCamera":
+                    if (divisao == null) System.out.println("Linha inválida.");
+                    SmartCamera sc = parseSmartCamera(linhaPartida[1]);
+                    assert casaMaisRecente != null;
+                    casaMaisRecente.addDevice(sc);
+                    casaMaisRecente.addToRoom(divisao, sc.getID());
+                    break;
                 default:
                     System.out.println("Linha inválida.");
                     break;
@@ -59,6 +72,33 @@ public class Parser {
         return new CasaInteligente(nif, nome, forn);
     }
 
+    public SmartBulb parseSmartBulb(String input){
+        String[] campos = input.split(",");
+        int tonalidade = Integer.parseInt(campos[0]);
+        double size = Double.parseDouble(campos[1]);
+        double consumo = Double.parseDouble(campos[2]);
+        return new SmartBulb(tonalidade, consumo, size);
+    }
 
-    //definir parser para cada classe(dispositivo)
+    public SmartSpeaker parseSmartSpeaker(String input){
+        String[] campos = input.split(",");
+        int volume = Integer.parseInt(campos[0]);
+        String channel = campos[1];
+        String marca = campos[2];
+        double consumo = Double.parseDouble(campos[3]);
+        return new SmartSpeaker(volume, channel, marca, consumo);
+
+    }
+
+
+    public SmartCamera parseSmartCamera(String input){
+        String[] campos = input.split(",");
+        String resolucao = campos[0];
+        int tamanho = Integer.parseInt(campos[1]);
+        double consumo = Double.parseDouble(campos[2]);
+        return new SmartCamera(resolucao, tamanho, consumo);
+    }
+
+
+
 }
