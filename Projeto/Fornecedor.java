69f3f639af6282e1386e5addaf7afe76a2784311
xDwake <gabi.alex.sil2002@gmail.com>
2022-05-17 17:59:22 +0100

    fodasse

--- Projeto/Fornecedor.java
+++ Projeto/Fornecedor.java
@@ -1,46 +1,61 @@
-import java.util.Objects;
+
+        import java.util.Objects;
 
 public class Fornecedor {
 
-        private String string;
+    private String string;
+    private int valorbase;
 
-        public Fornecedor(){
-            this.string = "";
-        }
+    public Fornecedor(){
+        this.string = "EDP Comercial";
+        this.valorbase = 4;
 
-        public Fornecedor(String string){
-            this.string = string;
-        }
+    }
 
-        public Fornecedor (Fornecedor forn){
-            this.string = forn.getString();
-        }
+    public Fornecedor(String string){
+        this.string = string;
+        this.valorbase = 4;
+    }
 
-        public String getString() {
-            return this.string;
-        }
+    public Fornecedor (Fornecedor forn){
+        this.string = forn.getString();
+        this.valorbase = forn.getValorBase();
+    }
 
-        public void setString(String string) {
-            this.string = string;
-        }
+    public int getValorBase() {
+        return this.valorbase;
+    }
 
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof Fornecedor)) return false;
-            Fornecedor that = (Fornecedor) o;
-            return this.string.equalsIgnoreCase(that.getString());
-        }
+    public void setValorBase(int vb){
+        this.valorbase = vb;
 
-        public String toString() {
-            return "Fornecedor{" +
-                    "fornecedor=" + string +
-                    '}';
-        }
+    }
 
-        
-        public Fornecedor clone(){
-            return new Fornecedor(this);
-        }
+    public String getString() {
+        return this.string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fornecedor)) return false;
+        Fornecedor that = (Fornecedor) o;
+        return this.string.equalsIgnoreCase(that.getString());
+    }
+
+    public String toString() {
+        return "Fornecedor{" +
+                "fornecedor=" + string +
+                "valor base=" + valorbase +
+                '}';
+    }
+
+
+    public Fornecedor clone(){
+        return new Fornecedor(this);
+    }
 
 }
-
