e2e8a1b47a512ea57a5b3e2e5bacc263f15b06af
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2022-05-18 19:30:45 +0100

    Create Main.java

---
+++ Projeto/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Controller controlador = new Controller();
+
+        controlador.run();
+
+
+
+
+    }
