3af3f94d8d248ed6557cb56b29efc9b02f831582
HugoRochartk <79595157+HugoRochartk@users.noreply.github.com>
2022-05-21 21:12:49 +0100

    Update Main.java

--- Projeto/Main.java
+++ Projeto/Main.java
@@ -1,16 +1,17 @@
 import java.util.*;
 
+/**
+ * A Main possuí a função que deve ser executada.
+ *
+ * @author Gabriel Silva, Hugo Rocha, José Faria
+ *
+ */
 
 public class Main {
     public static void main(String[] args) {
 
-
-
         Controller controlador = new Controller();
 
         controlador.run();
-
-
-
 
     }
